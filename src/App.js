6a24c4999ab4f4427d69812c4a6176446b09a448
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-29 22:20:58 -0300

    Add logo, and animation in photoCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ListOfCategories } from './components/listOfCategories/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/listOfPhotoCards/ListOfPhotoCards'
+import { Logo } from './components/logo/Logo'
 export const App = () => <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
 </>
