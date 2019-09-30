e93ec4114e8c431c764a2126e6e9e72677273bb6
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-29 23:03:03 -0300

    Start to consume the back, and add a fixed navbar when scroll

--- src/components/listOfCategories/ListOfCategories.js
+++ src/components/listOfCategories/ListOfCategories.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../category/Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 export const ListOfCategories = (props) => {
-    return (
-        <List>
+    const [categories, setCategories] = useState([])
+    const [showFixed, setShowFixed] = useState(false)
+
+    useEffect(() => {
+        fetch('https://petgram-api-guusy.now.sh/categories')
+            .then(res => res.json())
+            .then((response) => {
+                setCategories(response)
+            })
+    }, [])
+
+    useEffect(() => {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+
+        document.addEventListener('scroll', onScroll)
+
+        return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List className={fixed ? 'fixed' : ''}>
             {
                 categories.map(category =>
                     <Item key={category.id} >
                         <Category {...category} />
                     </Item>)
             }
-        </List>
+        </List>)
+    return (
+        <>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </>
     )
 }
