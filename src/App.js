d33ad3bc29258dc5720dc02cb739fd84dfa0e6aa
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-09-30 23:56:31 -0300

    add photoCard detail with dummy urlSearchParams and apply render props pattern to do graphql query

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import { ListOfCategories } from './components/listOfCategories/ListOfCategories
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/listOfPhotoCards/ListOfPhotoCards'
 import { Logo } from './components/logo/Logo'
-export const App = () => <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-</>
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+export const App = () => {
+    const urlParams = new window.URLSearchParams(window.location.search)
+    const detailId = urlParams.get('detail')
+    console.log(detailId)
+    return <>
+        <GlobalStyle />
+        <Logo />
+        {detailId ?
+            <PhotoCardWithQuery id={detailId} /> :
+            <>
+                <ListOfCategories />
+                <ListOfPhotoCards categoryId={2} />
+            </>}
+
+    </>
+}
