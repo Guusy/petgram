d3643927a9a9e6f075af9123f59ea158ed159186
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-10-01 23:49:15 -0300

    add user,favs page and render props userLogged function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/logo/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { User } from './pages/User'
+import { Favs } from './pages/Favs'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Router } from '@reach/router'
 import { Navbar } from './components/navBar/NavBar'
+
+const UserLogged = ({ children }) => {
+    return children({ isAuth: false })
+}
 export const App = () => {
     return <>
         <GlobalStyle />
@@ -15,6 +22,19 @@ export const App = () => {
             <Home path='/pet/:categoryId' />
             <Detail path='/detail/:detailId' />
         </Router>
+        <UserLogged>
+            {({ isAuth }) =>
+                isAuth ?
+                    <Router>
+                        <User path='/user' />
+                        <Favs path='/favs' />
+                    </Router> :
+                    <Router>
+                        <NotRegisteredUser path='/user' />
+                        <NotRegisteredUser path='/favs' />
+                    </Router>
+            }
+        </UserLogged>
         <Navbar />
     </>
 }
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotRegisteredUser = () => (
+    <h1>NotRegisterUser</h1>
+)
