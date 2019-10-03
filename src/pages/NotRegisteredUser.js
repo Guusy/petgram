3eaebfac34744d96414724e568f7fb55817347e6
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-10-02 23:21:05 -0300

    save the token in sessionStorage, add login form and start to use useContext hook

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,33 +1,58 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/userForm/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
-export const NotRegisteredUser = () => (
-    <Context.Consumer>
-        {
-            ({ activateAuth }) =>
-                <RegisterMutation>
-                    {
-                        (register, { data, loading, error }) => {
-                            const onSubmit = ({ email, password }) => {
-                                const input = { email, password }
-                                const variables = { input }
-                                register({ variables })
-                                    .then(() => {
-                                        activateAuth()
-                                    })
-                            }
-                            const errorMsg = error && 'El usuario ya existe o hay algun problema'
-
-                            return <UserForm
-                                disabled={loading}
-                                error={errorMsg}
-                                onSubmit={onSubmit}
-                                title='Registrarse' />
-                        }
+import { LoginMutation } from '../container/LoginMutation'
+export const NotRegisteredUser = () => {
+    const { activateAuth } = useContext(Context)
+    return <>
+        <RegisterMutation>
+            {
+                (register, { data, loading, error }) => {
+                    const onSubmit = ({ email, password }) => {
+                        const input = { email, password }
+                        const variables = { input }
+                        register({ variables })
+                            .then(({ data }) => {
+                                const { signup } = data
+                                activateAuth(signup)
+                            })
                     }
+                    const errorMsg = error && 'El usuario ya existe o hay algun problema'
 
-                </RegisterMutation>
-        }
-    </Context.Consumer>
-)
+                    return <UserForm
+                        disabled={loading}
+                        error={errorMsg}
+                        onSubmit={onSubmit}
+                        title='Registrarse' />
+                }
+            }
+
+        </RegisterMutation>
+        <LoginMutation>
+            {
+                (login, { data, loading, error }) => {
+                    const onSubmit = ({ email, password }) => {
+                        const input = { email, password }
+                        const variables = { input }
+                        login({ variables })
+                            .then(({ data }) => {
+                                const { login } = data
+                                activateAuth(login)
+                            })
+                    }
+                    const errorMsg = error && 'La contraseña no es correcta o el usuario no existe'
+
+                    return <UserForm
+                        disabled={loading}
+                        error={errorMsg}
+                        onSubmit={onSubmit}
+                        title='Iniciar sesion' />
+                }
+            }
+
+        </LoginMutation>
+    </>
+}
+
+
