c47f31d75b4ac9ad5b1936123a45e9dbb3508ffe
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-10-03 23:36:31 -0300

    Start to use context hook in app.js and define login and 404 pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/logo/Logo'
 import { Home } from './pages/Home'
@@ -6,33 +6,29 @@ import { Detail } from './pages/Detail'
 import { User } from './pages/User'
 import { Favs } from './pages/Favs'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 import { Navbar } from './components/navBar/NavBar'
-import Context from './Context'
+import { Context } from './Context'
 
 export const App = () => {
+    const { isAuth } = useContext(Context)
     return <>
         <GlobalStyle />
         <Logo />
         <Router>
+            <NotFound default />
             <Home path='/' />
             <Home path='/pet/:categoryId' />
             <Detail path='/detail/:detailId' />
+            {!isAuth && <NotRegisteredUser path='/login' />}
+            {!isAuth && <Redirect from='/favs' to='/login' />}
+            {!isAuth && <Redirect from='/user' to='/login' />}
+            {isAuth && <Redirect from='/login' to='/' />}
+            <User path='/user' />
+            <Favs path='/favs' />
         </Router>
-        <Context.Consumer>
-            {({ isAuth }) =>
-                isAuth ?
-                    <Router>
-                        <User path='/user' />
-                        <Favs path='/favs' />
-                    </Router> :
-                    <Router>
-                        <NotRegisteredUser path='/user' />
-                        <NotRegisteredUser path='/favs' />
-                    </Router>
-            }
-        </Context.Consumer>
         <Navbar />
     </>
 }
